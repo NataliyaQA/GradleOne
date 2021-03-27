144d982b3dbc4eb12317ca885c765e44d06f902f
Nataliya Malynka <nataliya_malynka@epam.com>
2021-03-27 11:33:30 +0200

    Fixed tests "CreateAnAccount" (re-written with new methods located in "SignInPage"

--- src/main/java/pageObjects/SignInPage.java
+++ src/main/java/pageObjects/SignInPage.java
@@ -14,6 +14,7 @@ public class SignInPage {
     WebElement createAnAccountText;
     WebElement createAnAccountButton;
     WebElement emailAddressFieldCreateAnAccount;
+    WebElement errorExistingEmail;
     WebElement alreadyRegisteredTitle;
     WebElement emailAddressFieldAlreadyRegistered;
     WebElement passwordFieldAlreadyRegistered;
@@ -28,188 +29,140 @@ public class SignInPage {
     WebElement youTubeIcon;
     WebElement gmailIcon;
 
-    String actualTitle;
-    String expectedTitle = "Login - My Store";
-    String actualUrl;
-    String expectedUrl = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+    String pageTitle;
+    String pageUrl;
 
-    String actualSignInHeaderButtonName;
-    String expectedSignInHeaderButtonName = "Sign in";
-    String actualAuthenticationSectionTitle;
-    String expectedAuthenticationSectionTitle = "Authentication";
-    String actualCreateAnAccountTitleName;
-    String expectedCreateAnAccountTitleName = "Create an account";
-    String actualCreateAnAccountText;
-    String expectedCreateAnAccountText = "Please enter your email address to create an account.";
-    String actualCreateAnAccountButtonName;
-    String expectedCreateAnAccountButtonName = "Create an account";
-    String actualEmailAddressFieldCreateAnAccountName;
-    String expectedEmailAddressFieldCreateAnAccountName = "Email address";
-    String actualAlreadyRegisteredTitle;
-    String expectedAlreadyRegisteredTitle = "Already registered?";
-    String actualEmailAddressFieldAlreadyRegisteredName;
-    String expectedEmailAddressFieldAlreadyRegisteredName = "Email address";
-    String actualPasswordFieldAlreadyRegisteredName;
-    String expectedPasswordFieldAlreadyRegisteredName = "Password";
-    String actualSignInAlreadyRegisteredButtonName;
-    String expectedSignInAlreadyRegisteredButtonName = "Sign in";
-    String actualForgotYourPasswordName;
-    String expectedForgotYourPasswordName = "Forgot your password?";
-    //String email;
-    String email = "Auto_" + RandomStringUtils.randomAlphabetic(5) + "mail.com";
+    public static final String EXPECTED_TITLE = "Login - My Store";
 
+    public static final String EXPECTED_URL = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+
+    public static final String EXPECTED_SIGN_IN_HEADER_BUTTON_NAME = "Sign in";
+
+    public static final String EXPECTED_AUTHENTICATION_SECTION_TITLE = "Authentication";
+
+    public static final String EXPECTED_CREATE_AN_ACCOUNT_TITLE_NAME = "Create an account";
+
+    public static final String EXPECTED_CREATE_AN_ACCOUNT_TEXT = "Please enter your email address to create an account.";
+
+    public static final String EXPECTED_CREATE_AN_ACCOUNT_BUTTON_NAME = "Create an account";
+
+    public static final String EXPECTED_EMAIL_ADDRESS_FIELD_CREATE_AN_ACCOUNT_NAME = "Email address";
+
+    public static final String EXPECTED_ERROR_EXISTING_EMAIL = "An account using this email address has already been " +
+            "registered. Please enter a valid password or request a new one.";
+
+    public static final String EXPECTED_ALREADY_REGISTERED_TITLE = "Already registered?";
+
+    public static final String EXPECTED_EMAIL_ADDRESS_FIELD_ALREADY_REGISTERED_NAME = "Email address";
+
+    public static final String EXPECTED_PASSWORD_FIELD_ALREADY_REGISTERED_NAME = "Password";
+
+    public static final String EXPECTED_SIGN_IN_ALREADY_REGISTERED_BUTTON_NAME = "Sign in";
+
+    public static final String EXPECTED_FORGOT_YOUR_PASSWORD_NAME = "Forgot your password?";
+
+    String emailO = "Auto_" + RandomStringUtils.randomAlphabetic(5) + "@mail.com";
+
+    //constructor
     public SignInPage(WebDriver driver) {
         this.driver = driver;
 
-        actualTitle = driver.getTitle();                //get title of Sign in page
-        actualUrl = driver.getCurrentUrl();             //get URL for further comparison
+        //emailO = "Auto_" + RandomStringUtils.randomAlphabetic(5) + "@mail.com";
+    }
 
+    //methods
+    public String actualTitleFind(WebDriver driver) { //get title of Sign in page
+        pageTitle = driver.getTitle();
+        return pageTitle;
+    }
+
+    public String actualUrlFind(WebDriver driver) {  //get URL for further comparison
+        pageUrl = driver.getCurrentUrl();
+        return pageUrl;
+    }
+
+    public String signInHeaderButtonNameFind(WebDriver driver) {
         signInHeaderButton = driver.findElement(By.xpath("//a[@title='Log in to your customer account']"));
-        actualSignInHeaderButtonName = signInHeaderButton.getText();  //get name of button
+        return signInHeaderButton.getText();
+    }
 
+    public String authenticationSectionTitleFind(WebDriver driver) {
         authenticationSectionTitle = driver.findElement(By.xpath("//h1[text()='Authentication']"));
-        actualAuthenticationSectionTitle = authenticationSectionTitle.getText();
+        return authenticationSectionTitle.getText();
+    }
 
+    public String createAnAccountTitleFind(WebDriver driver) {
         createAnAccountTitle = driver.findElement(By.xpath("//h3[text()='Create an account']"));
-        actualCreateAnAccountTitleName = createAnAccountTitle.getText();
+        return createAnAccountTitle.getText();
+    }
 
+    public String createAnAccountTextFind(WebDriver driver) {
         createAnAccountText = driver.findElement(By.xpath("//h3[contains(text(),'Create')]"));
-        actualCreateAnAccountText = createAnAccountText.getText();
+        return createAnAccountText.getText();
+    }
 
+    public String createAnAccountButtonFind(WebDriver driver) {
         createAnAccountButton = driver.findElement(By.xpath("//*[@id=\"SubmitCreate\"]/span"));
-        actualCreateAnAccountButtonName = createAnAccountButton.getText();
+        return createAnAccountButton.getText();
+    }
 
+    public String emailAddressFieldCreateAnAccountFind(WebDriver driver) {
         emailAddressFieldCreateAnAccount = driver.findElement(By.xpath("//*[@id=\"email_create\"]"));
-        actualEmailAddressFieldCreateAnAccountName = emailAddressFieldCreateAnAccount.getText();
+        return emailAddressFieldCreateAnAccount.getText();
+    }
 
+    public String errorMessageIsExist(WebDriver driver) {
+        errorExistingEmail = driver.findElement(By.cssSelector("#create_account_error > ol > li"));
+        return errorExistingEmail.getText();
+    }
+
+    public String alreadyRegisteredTitleFind(WebDriver driver) {
         alreadyRegisteredTitle = driver.findElement(By.xpath("//h3[text()='Already registered?']"));
-        actualAlreadyRegisteredTitle = alreadyRegisteredTitle.getText();
+        return alreadyRegisteredTitle.getText();
+    }
 
+    public String emailAddressFieldAlreadyRegisteredFind(WebDriver driver) {
         emailAddressFieldAlreadyRegistered = driver.findElement(By.xpath("//*[@id=\"email\"]"));
-        actualEmailAddressFieldAlreadyRegisteredName = emailAddressFieldAlreadyRegistered.getText();
+        return emailAddressFieldAlreadyRegistered.getText();
+    }
 
+    public String passwordFieldAlreadyRegisteredFind(WebDriver driver) {
         passwordFieldAlreadyRegistered = driver.findElement(By.xpath("//*[@id=\"passwd\"]"));
-        actualPasswordFieldAlreadyRegisteredName = passwordFieldAlreadyRegistered.getText();
+        return passwordFieldAlreadyRegistered.getText();
+    }
 
+    public String signInAlreadyRegisteredButtonFind(WebDriver driver) {
         signInAlreadyRegisteredButton = driver.findElement(By.xpath("//*[@id=\"SubmitLogin\"]/span"));
-        actualSignInAlreadyRegisteredButtonName = signInAlreadyRegisteredButton.getText();
+        return signInAlreadyRegisteredButton.getText();
+    }
 
+    public String forgotYourPasswordFind(WebDriver driver) {
         forgotYourPassword = driver.findElement(By.xpath("//p[contains(@class,'lost')]/a"));
-        actualForgotYourPasswordName = forgotYourPassword.getText();
+        return forgotYourPassword.getText();
     }
 
     public SignInPage clickSignInHeaderButton() {
+        signInHeaderButton = driver.findElement(By.xpath("//a[@title='Log in to your customer account']"));
         signInHeaderButton.click();
         return this;
     }
 
-    public String getActualTitle() {
-        return actualTitle;
+    public SignInPage createAnAccountButtonClick() {
+        createAnAccountButton = driver.findElement(By.xpath("//*[@id='SubmitCreate']/span"));
+        createAnAccountButton.click();
+        return this;
     }
 
-    public String expectedTitle() {
-        return expectedTitle;
+    public SignInPage inputEmail() {
+        emailAddressFieldCreateAnAccount = driver.findElement(By.xpath("//*[@id='email_create']"));
+        emailAddressFieldCreateAnAccount.sendKeys(emailO);
+        return this;
     }
 
-    public String getActualUrl() {
-        return actualTitle;
-    }
-
-    public String expectedUrl() {
-        return expectedUrl;
-    }
-
-    public String getActualSignInHeaderButtonName() {
-        return actualSignInHeaderButtonName;
-    }
-
-    public String expectedSignInHeaderButtonName() {
-        return expectedSignInHeaderButtonName;
-    }
-
-    public String getActualAuthenticationSectionTitle() {
-        return actualAuthenticationSectionTitle;
-    }
-
-    public String expectedAuthenticationSectionTitle() {
-        return expectedAuthenticationSectionTitle;
-    }
-
-    public String getActualCreateAnAccountTitleName() {
-        return actualCreateAnAccountTitleName;
-    }
-
-    public String expectedCreateAnAccountTitleName() {
-        return expectedCreateAnAccountTitleName;
-    }
-
-    public String getActualCreateAnAccountText() {
-        return actualCreateAnAccountText;
-    }
-
-    public String expectedCreateAnAccountText() {
-        return expectedCreateAnAccountText;
-    }
-
-    public String getActualCreateAnAccountButtonName() {
-        return actualCreateAnAccountButtonName;
-    }
-
-    public String expectedCreateAnAccountButtonName() {
-        return expectedCreateAnAccountButtonName;
-    }
-
-    public String getActualEmailAddressFieldCreateAnAccountName() {
-        return actualEmailAddressFieldCreateAnAccountName;
-    }
-
-    public String expectedEmailAddressFieldCreateAnAccountName() {
-        return expectedEmailAddressFieldCreateAnAccountName;
-    }
-
-    public String getActualAlreadyRegisteredTitle() {
-        return actualAlreadyRegisteredTitle;
-    }
-
-    public String expectedAlreadyRegisteredTitle() {
-        return expectedAlreadyRegisteredTitle;
-    }
-
-    public String getActualEmailAddressFieldAlreadyRegisteredName() {
-        return actualEmailAddressFieldAlreadyRegisteredName;
-    }
-
-    public String expectedEmailAddressFieldAlreadyRegisteredName() {
-        return expectedEmailAddressFieldAlreadyRegisteredName;
-    }
-
-    public String getActualPasswordFieldAlreadyRegisteredName() {
-        return actualPasswordFieldAlreadyRegisteredName;
-    }
-
-    public String expectedPasswordFieldAlreadyRegisteredName() {
-        return expectedPasswordFieldAlreadyRegisteredName;
-    }
-
-    public String getActualSignInAlreadyRegisteredButtonName() {
-        return actualSignInAlreadyRegisteredButtonName;
-    }
-
-    public String expectedSignInAlreadyRegisteredButtonName() {
-        return expectedSignInAlreadyRegisteredButtonName;
-    }
-
-    public String getActualForgotYourPasswordName() {
-        return actualForgotYourPasswordName;
-    }
-
-    public String expectedForgotYourPasswordName() {
-        return expectedForgotYourPasswordName;
-    }
-
+    //Overloading
     public SignInPage inputEmail(String email) {
-            emailAddressFieldCreateAnAccount.sendKeys(email);
-            return this;
-        }
-
+        emailAddressFieldCreateAnAccount = driver.findElement(By.xpath("//*[@id='email_create']"));
+        emailAddressFieldCreateAnAccount.sendKeys(email);
+        return this;
+    }
 }
--- src/test/java/com/automationpractice/CreateAnAccount.java
+++ src/test/java/com/automationpractice/CreateAnAccount.java
@@ -1,13 +1,90 @@
 package com.automationpractice;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObjects.SignInPage;
 
 public class CreateAnAccount extends BaseTestAbstract{
 
-    @Test
-    public void createAnAccount() {
-        new SignInPage(driver).clickSignInHeaderButton();
+    @Test // works
+    public void createAnAccountTitle() throws InterruptedException {
+        String stringUrl = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+        driver.get(stringUrl);
+
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.clickSignInHeaderButton(); //works with and without
+
+        String actualTitle = signInPage.actualTitleFind(driver);
+        String expectedTitle = signInPage.EXPECTED_TITLE;
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+        System.out.println("You are on Login - My Store");
+    }
+
+    @Test //works
+    public void emailInputWithOneNew() {
+        String stringUrl = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+        driver.get(stringUrl);
+
+        SignInPage signInPage = new SignInPage(driver);
+
+        signInPage.emailAddressFieldCreateAnAccountFind(driver);
+        signInPage.inputEmail();
+        signInPage.createAnAccountButtonClick();
+    }
+
+    @Test // works
+    public void emailInput() throws InterruptedException {
+        String stringUrl = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+        driver.get(stringUrl);
+
+        new SignInPage(driver).emailAddressFieldCreateAnAccountFind(driver);
+        new SignInPage(driver).inputEmail();
+        new SignInPage(driver).createAnAccountButtonClick();
+
+    }
+
+    @Test // works
+    public void emailInputSameEmail() throws InterruptedException {
+        String stringUrl = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+        driver.get(stringUrl);
+
+        SignInPage signInPage = new SignInPage(driver);
+        Thread.sleep(5000);
+        String email = "qwe@gmail.com";
+
+        signInPage.inputEmail(email);
+        signInPage.createAnAccountButtonClick();
+        Thread.sleep(5000);
+
+//        driver.get(stringUrl);
+//        SignInPage signInPageTwo = new SignInPage(driver);
+//        signInPageTwo.inputEmail(email);
+//        Thread.sleep(5000);
+//        signInPage.createAnAccountButtonClick();
+//        Thread.sleep(5000);
+
+        // 2d part
+        Thread.sleep(5000);
+        String actual = signInPage.errorMessageIsExist(driver);
+        String expected = signInPage.EXPECTED_ERROR_EXISTING_EMAIL;
+
+        Assert.assertEquals(actual, expected);
+        System.out.println("An account using this email address has already been registered. " +
+                "Please enter a valid password or request a new one.");
+
+    }
+
+    @Test // I want to add put from ContextMap
+    public void emailInputWithPut() {
+
+        String stringUrl = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+        driver.get(stringUrl);
+
+        new SignInPage(driver).emailAddressFieldCreateAnAccountFind(driver);
+        new SignInPage(driver).inputEmail();
+        //put....
+        new SignInPage(driver).createAnAccountButtonClick();
     }
 
 }
